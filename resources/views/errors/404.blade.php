ef410334ffdc027e1b06b0f65de0f74a53a7856c
melifes <zhanglin-pc@live.com>
2016-05-17 00:04:33 +0800

    update 404 page style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,61 +2,14 @@
 <html>
 <head>
     <title>Oops! 404 Error</title>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <style>
-        html, body {
-            height: 100%;
-        }
-        *,:after,:before {
-            box-sizing: border-box;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            font-weight: 500;
-            font-family: 'Lato';
-        }
-
-        .container {
-            height: 100%;
-            text-align: center;
-            margin: 0 auto;
-            position: relative;
-        }
-
-        .content {
-            padding-top: 10em;
-        }
-
-        .title {
-            font-size: 32px;
-            margin: 1em 0;
-        }
-        .content a {
-            font-size: 18px;
-            color: #5FCF80;
-            text-decoration: none;
-        }
-        a span {
-            color: #C792E3;
-        }
-        .footer {
-
-        }
-        .footer img {
-            position: absolute;
-            bottom: 0;
-            left: 12.9999%;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/home/errors.css">
 </head>
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Oops! The requested URL was not found on this system</div>
-        <a href="{{ url('/') }}"><span>return</span> {{ url('/') }}</a>
+        <div class="title">Oops!</div>
+        <div class="desc">The requested URL was not found on this system</div>
+        { <a href="{{ url('/') }}"><span>return</span> {{ url('/') }}</a> }
     </div>
     <div class="footer">
         <img src="/images/404.png" alt="404 notfound">
